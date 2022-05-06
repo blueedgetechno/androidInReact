0d48a1da5adfe8552c05302d75a73aac62ae8c04
andrewstech <info@andrewstech.me>
2022-05-06 19:29:14 +0100

    Add sentry

--- src/index.js
+++ src/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import store from './store';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import { Provider } from 'react-redux';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+Sentry.init({
+  dsn: "https://202b09d8695844e89470727c1c381957@o575799.ingest.sentry.io/6385094",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
